1af91ea97c7d95f227404c62d10035cadade9160
AstroidusTv <asteroidus@protonmail.com>
2023-05-28 14:23:57 +0200

    fix bug

--- main.sh
+++ main.sh
@@ -3,19 +3,19 @@
 file="./data/new_config.conf"
 chmod +x "./code/config.sh" "./code/create.sh" "new-project.desktop"
 
-#!/bin/bash
-
 app_dir="$HOME/.local/share/applications"
 new_project_dir="new-project.desktop"
 
-# Move the new-project directory to /Desktop
-mv "$new_project_dir" "$desktop_dir/"
 
 if [ -s "$file" ]; then
-    # Si le fichier n'est pas vide, exécuter une commande
+    # Execute create.sh
     ./code/create.sh
+    
+
 else
-    # Si le fichier est vide, exécuter une autre commande
+    # Execute confing.sh and create.sh
+    # Move the new-project directory to /Desktop
+    mv "$new_project_dir" "$desktop_dir/"
     ./code/config.sh
     ./code/create.sh
 fi
